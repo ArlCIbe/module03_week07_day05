79201cf1df380da499f938e2a845d0edba342024
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-04-01 12:20:20 -0500

    completed part 1

--- Part1/index.js
+++ Part1/index.js
@@ -1,17 +1,42 @@
 //Part 1-1
-console.log("Index.js linked successfully")
+console.log("Index.js was linked successfully")
 
 let showPunchlineButtonHTML = document.querySelector("#showPunchlineButton");
 showPunchlineButtonHTML.addEventListener("click", function () {
     //Part 1-2
 
+    //select the punchline element
+    let punchlineHTML = document.getElementById("jokePunchline")
+
+    //change the value of its visibility to visible
+    punchlineHTML.style.visibility = "visible"
+
+
 });
+
 
 
 let refreshJokeButtonHTML = document.querySelector("#refreshJokeButton");
 refreshJokeButtonHTML.addEventListener("click", function () {
     //Part 1-3
+    console.log("Refreshing Joke..")
+    fetch("https://official-joke-api.appspot.com/random_joke")
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+
+            let jokeSetupHTML = document.getElementById("jokeSetup")
+            jokeSetupHTML.innerHTML = data.setup
+
+            //reset the value of the punchline html element
+            let jokePunchlineHTMl = document.getElementById("jokePunchline")
+            jokePunchlineHTMl.innerHTML = data.punchline
 
 
+
+            jokePunchlineHTMl.style.visibility = ""
+        })
 
 });
